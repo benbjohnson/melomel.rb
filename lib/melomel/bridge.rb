6d6c7e102712a3f86cc2ce91239ccdda6db2b8af
Ben Johnson <benbjohnson@yahoo.com>
2011-04-08 06:47:40 -0600

    Add open domain access to policy file. (via GogKurt)

--- lib/melomel/bridge.rb
+++ lib/melomel/bridge.rb
@@ -67,6 +67,7 @@ module Melomel
       policy << '<!DOCTYPE cross-domain-policy SYSTEM "http://www.macromedia.com/xml/dtds/cross-domain-policy.dtd">'
       policy << '<cross-domain-policy>'
       policy << "<allow-access-from domain=\"#{host}\" to-ports=\"#{port}\"/>"
+      policy << "<allow-access-from domain=\"*\" to-ports=\"#{port}\"/>"
       policy << '</cross-domain-policy>'
       socket.send(policy, 0)
       socket.flush()
